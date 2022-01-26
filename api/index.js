e6718c847b45101c3291145d68f65885223f6a74
klinsman <joseklinsman259@gmail.com>
2022-01-26 13:55:43 -0300

    first commit

---
+++ api/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+const crud = require('./database/crud')
+const redis = require('./redisDatabase/redis')
+
+
+//http://localhost:3000/updateUser/klinsman@gmail.com/name/klinsman/email/klinsman@gmail.com/password/123
+app.get('/newUser/:name/email/:email/password/:password', crud.NewUser);
+app.get('/updateUser/:usrID/name/:name/email/:email/password/:password', crud.UpdateUser);
+app.get('/deleteUser/:usrID/', crud.DeleteUser);
+
+app.get('/writeSketch/:usrID/text/:text', redis.WriteSketch);
+app.get('/readSketch/:usrID', redis.ReadSketch);
+
+app.listen(process.env.PORT, () => { 
+  console.log(`http://localhost:${process.env.PORT}`); 
+});
---
+++ api/redisDatabase/client.js
@@ -0,0 +1,12 @@
+const Redis = require("ioredis");
+
+const client = new Redis(process.env.REDIS_PORT, process.env.REDIS_HOST)
+
+client.on("connect", function (error){
+    console.log("conectado")
+})
+client.on("error", function (error){
+    console.log(error)
+})
+
+module.exports = client;
